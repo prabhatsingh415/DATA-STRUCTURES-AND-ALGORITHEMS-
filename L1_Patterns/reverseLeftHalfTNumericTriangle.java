55ddab0846ea8979a1298a61a4666f48d2288a5c
prabhatsingh415 <149043789+prabhatsingh415@users.noreply.github.com>
2024-07-03 20:51:51 +0530

    Update and rename reverseLeftHalfTrinagle.java to reverseLeftHalfTNumericTriangle.java
    fix error in code

--- L1_Patterns/reverseLeftHalfTNumericTriangle.java
+++ L1_Patterns/reverseLeftHalfTNumericTriangle.java
@@ -2,7 +2,7 @@ package L1_Patterns;
 
 import java.util.Scanner;
 
-public class reverseLeftHalfTrinagle {
+public class reverseLeftHalfNumericTriangle{
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
@@ -10,10 +10,9 @@ public class reverseLeftHalfTrinagle {
         System.out.println("Enter The Numbers  Of Rows And Columns");
         int rows = scan.nextInt();               // Taking Numbers Of Rows As Input
 
-        for (int j = 1;j<=rows ;j++)   {
-            for (int i = 1; i <=rows ; i++) {
+        for (int j = 5;j>=1 ;j--)   {
+            for (int i = 1; i <=j ; i++) {
                 System.out.print(" "+i +" ");             // Loop Printing Star Pattern
-
 
             }
             System.out.println();                        // Gives New Line After Every Iteration
@@ -21,3 +20,4 @@ public class reverseLeftHalfTrinagle {
         }
     }
 }
+
